f28deabca2f0d8127ea079c6a74178c59d6d8a6b
Artemy Gibson <artemygibson@gmail.com>
2023-09-04 16:21:29 -0700

    Edit tests: want service to only return Faraday responses now

--- spec/services/movies_service_spec.rb
+++ spec/services/movies_service_spec.rb
@@ -2,35 +2,35 @@ require 'rails_helper'
 
 RSpec.describe MoviesService, :vcr do
   it 'can retrieve top 20 movies' do
-    movies = MoviesService.new.top_rated
-    expect(movies).to be_an(Array)
-    expect(movies.count).to eq(20)
+    response = MoviesService.new.top_rated
+    expect(response).to be_a(Faraday::Response)
+    expect(response.env[:url].to_s).to include("top_rated")
+
+    movies = JSON.parse(response.body, symbolize_names: true)
+    expect(movies[:results].count).to eq(20)
   end
 
-  it 'can search for movies by keyword in title', :vcr do
-    service = MoviesService.new
-    keyword = 'Inception'
-    movies = service.search(keyword)
+  it 'can search for movies by keyword in title' do
+    response = MoviesService.new.search('inception')
 
-    expect(movies).to be_an(Array)
+    expect(response).to be_an(Faraday::Response)
+
+    movies = JSON.parse(response.body, symbolize_names: true)[:results]
     expect(movies.first).to have_key(:title)
-    
+
     titles = movies.map { |movie| movie[:title].downcase }
-    expect(titles.any? { |title| title.include?(keyword.downcase) }).to be(true)
+    expect(titles.all? { |title| title.include?('inception') }).to be(true)
   end
 
-  it 'can generate a poro for a specific movie' do
-    movie = MoviesService.new.find_movie(234)
-    expect(movie).to be_a(Movie)
-    expect(movie.title).to be_a(String)
-    expect(movie.id).to be_an(Integer)
-    expect(movie.image).to be_a(String)
-    expect(movie.rating).to be_a(Float)
-    expect(movie.runtime).to be_an(Integer)
-    expect(movie.genres).to be_a(String)
-    expect(movie.summary).to be_a(String)
-    expect(movie.cast).to all be_a(CastMember)
-    expect(movie.total_reviews).to be_an(Integer)
-    expect(movie.reviews).to all be_a(Review)
+  it 'can search for a single movie by ID' do
+    response = MoviesService.new.find_movie(234)
+    expect(response).to be_a(Faraday::Response)
+    
+    movie = JSON.parse(response.body, symbolize_names: true)
+
+    expect(movie).to be_a(Hash)
+    expect(movie).to have_key(:title)
+    expect(movie).to have_key(:reviews)
+    expect(movie).to have_key(:credits)
   end
 end
