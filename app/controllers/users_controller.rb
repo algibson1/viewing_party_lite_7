201a8006254f3274fd025f626a1915f2ca797a5e
Artemy Gibson <artemygibson@gmail.com>
2023-09-04 14:51:38 -0700

    Move movies methods into MoviesController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,41 +26,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:user_id])
   end
 
-  def movies
-    @user = User.find(params[:user_id])
-    query = params[:q]
-
-    movies_data = if query == 'top_rated'
-                    MoviesService.new.top_rated
-                  elsif query.present?
-                    MoviesService.new.search(query)
-                  else
-                    []
-                  end
-
-    @movies = movies_data.map { |movie_data| Movie.new(movie_data) }
-    @title = if query == 'top_rated'
-               'Top Rated Movies'
-             elsif query.present?
-               "Search Results for '#{query}'"
-             else
-               'Error: No Query'
-             end
-    @movies = movies_data.map { |movie_data| Movie.new(movie_data) }
-  end
-
-  def movie_show
-    @user = User.find(params[:user_id])
-    @movie = facade.find_movie(params[:movie_id])
-  end
-
   private
 
   def user_params
     params.require(:user).permit(:name, :email)
-  end
-
-  def facade
-    @facade ||= MoviesFacade.new
   end
 end
