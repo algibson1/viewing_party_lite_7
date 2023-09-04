7571c104c33da740d9a8215160f08c4111283ff6
Artemy Gibson <artemygibson@gmail.com>
2023-09-04 16:22:10 -0700

    Removing all parsing

--- app/services/movies_service.rb
+++ app/services/movies_service.rb
@@ -3,19 +3,16 @@ class MoviesService
     response = connection.get('3/search/movie') do |faraday|
       faraday.params['query'] = keyword
     end
-    JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
   def top_rated
     response = connection.get('3/movie/top_rated')
-    JSON.parse(response.body, symbolize_names: true)[:results]
   end
 
   def find_movie(id)
     response = connection.get("3/movie/#{id}") do |f|
       f.params['append_to_response'] = 'reviews,credits'
     end
-    Movie.new(JSON.parse(response.body, symbolize_names: true))
   end
 
   private
