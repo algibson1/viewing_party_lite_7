d8eee03eb3399ef5892f7240fca5f938f9444be5
Artemy Gibson <artemygibson@gmail.com>
2023-09-04 17:26:37 -0700

    Add tests for new methods to search by query

--- spec/facades/movies_facade_spec.rb
+++ spec/facades/movies_facade_spec.rb
@@ -17,4 +17,24 @@ RSpec.describe MoviesFacade, :vcr do
     expect(movie2).to eq(movie2_renamed)
     expect(facade._cached_movies).to eq({ 234 => movie1, 550 => movie2 })
   end
+
+  it 'can return a list of top_rated movies' do
+    facade = MoviesFacade.new
+    top_movies = facade.movies_list('top_rated')
+    expect(top_movies).to be_an(Array)
+    expect(top_movies).to all be_a(Movie)
+    expect(top_movies.count).to eq(20)
+    
+    expect(top_movies.all? { |movie| movie.rating > 8}).to eq(true)
+  end
+
+  it 'can search for movies by a query term' do
+    facade = MoviesFacade.new
+    movies = facade.movies_list('Inception')
+    expect(movies).to be_an(Array)
+    expect(movies).to all be_a(Movie)
+    expect(movies.count <= 20).to eq(true)
+    
+    expect(movies.all? { |movie| movie.title.include?('Inception')}).to eq(true)
+  end
 end
