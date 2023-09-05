4a943f7001dac040b9c782f31a46f75f67c8a65c
Artemy Gibson <artemygibson@gmail.com>
2023-09-05 13:39:32 -0700

    Add validations for password, edit formatting on parties methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,21 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :email
+  validates_presence_of :name, :email, :password_digest
   validates :email, uniqueness: { case_sensitive: false }
   has_many :party_guests
   has_many :viewing_parties, through: :party_guests
 
+  has_secure_password
+
   def hosted_parties
-    viewing_parties.joins(:party_guests).where('party_guests.user_id = ? AND party_guests.host = ?', id, true).distinct
+    viewing_parties.joins(:party_guests)
+      .where('party_guests.user_id = ? AND party_guests.host = ?', id, true)
+      .distinct
   end
 
   def invited_parties
-    viewing_parties.joins(:party_guests).where('party_guests.user_id = ? AND party_guests.host = ?', id,
-                                               false).distinct
+    viewing_parties.joins(:party_guests)
+      .where('party_guests.user_id = ? AND party_guests.host = ?', id, false)
+      .distinct
   end
 
   def friends
