724f9ba88c83b5c5c586c8b7ea8a2114c4d68436
Artemy Gibson <artemygibson@gmail.com>
2023-09-04 17:26:16 -0700

    Add functionality to search for movies by a query

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -6,12 +6,34 @@ class MoviesFacade
   end
 
   def find_movie(id)
-    @_cached_movies[id] ||= service.find_movie(id)
+    @_cached_movies[id] ||= get_movie(id)
   end
 
+  def movies_list(query_term)
+    return formatted(top_rated) if query_term == 'top_rated'
+    formatted(search(query_term))
+  end
+  
   private
-
   def service
-    MoviesService.new
+    @_service = MoviesService.new
+  end
+
+  def get_movie(id)
+    response = service.find_movie(id)
+    Movie.new(JSON.parse(response.body, symbolize_names: true))
+  end
+
+  def top_rated
+    @_top_rated ||= service.top_rated
+  end
+
+  def search(query_term)
+    service.search(query_term)
+  end
+
+  def formatted(response)
+    movie_data = JSON.parse(response.body, symbolize_names: true)[:results]
+    movie_data.map { |data| Movie.new(data) }
   end
 end
