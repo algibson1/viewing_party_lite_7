6b07f3abb64d8a5e091fb955e9b02c48f8796489
Artemy Gibson <artemygibson@gmail.com>
2023-09-05 13:37:06 -0700

    Add validation testing for password and password confirmation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,15 @@ RSpec.describe User do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of :password_digest }
+    it { should have_secure_password }
+
+    it 'encrypts passwords' do
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+      expect(user).to_not have_attribute(:password)
+      expect(user).to_not have_attribute(:password_confirmation)
+      expect(user.password_digest).to_not eq('password123')
+    end
   end
 
   describe 'relationships' do
@@ -13,10 +22,10 @@ RSpec.describe User do
   end
 
   it 'can find all users besides the current logged in one' do
-    @ally = User.create!(name: 'Ally Jean', email: 'allyjean@example.com')
-    @jimmy = User.create!(name: 'Jimmy Jean', email: 'jimmyjean@example.com')
-    @bobby = User.create!(name: 'Bobby Jean', email: 'bobbyjean@example.com')
-    @dennis = User.create!(name: 'Dennis Jean', email: 'dennisjean@example.com')
+    @ally = User.create!(name: 'Ally Jean', email: 'allyjean@example.com', password: 'password1', password_confirmation: 'password1')
+    @jimmy = User.create!(name: 'Jimmy Jean', email: 'jimmyjean@example.com', password: 'password2', password_confirmation: 'password2')
+    @bobby = User.create!(name: 'Bobby Jean', email: 'bobbyjean@example.com', password: 'password3', password_confirmation: 'password3')
+    @dennis = User.create!(name: 'Dennis Jean', email: 'dennisjean@example.com', password: 'password4', password_confirmation: 'password4')
     expect(@ally.friends).to match_array([@jimmy, @bobby, @dennis])
     expect(@jimmy.friends).to match_array([@ally, @bobby, @dennis])
     expect(@bobby.friends).to match_array([@jimmy, @ally, @dennis])
