dcc845d30719de14afbf5ef2b752620eb9019b96
Artemy Gibson <artemygibson@gmail.com>
2023-09-04 19:49:13 -0700

    Add find_user action

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -1,22 +1,28 @@
 class ViewingPartyController < ApplicationController
+  before_action :find_user
+
   def new
-    @user = User.find(params[:user_id])
     @movie = facade.find_movie(params[:movie_id])
   end
 
   def create
+    # Do some error handling instead?
     party = ViewingParty.new(party_params)
     if party.save
       party.send_invites(params[:user_id], guest_hash)
-      redirect_to user_path(params[:user_id])
+      redirect_to user_path(@user)
       flash[:success] = 'Party Created Successfully'
     else
-      redirect_to new_user_movie_viewing_party_path(params[:user_id], params[:movie_id])
+      redirect_to new_user_movie_viewing_party_path(@user, params[:movie_id])
       flash[:alert] = "Error: #{party.errors.full_messages.to_sentence}"
     end
   end
 
   private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
 
   def party_params
     params.permit(:duration, :party_date, :start_time, :movie_id)
@@ -24,9 +30,5 @@ class ViewingPartyController < ApplicationController
 
   def guest_hash
     params[:guests].permit!.to_h
-  end
-
-  def facade
-    @facade ||= MoviesFacade.new
   end
 end
