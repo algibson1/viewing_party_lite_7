060c4173f99dfe6e3a9cc333bfe1041b6a949948
Artemy Gibson <artemygibson@gmail.com>
2023-09-04 19:49:50 -0700

    Change to alternate syntax

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     get '/discover', to: 'users#discover'
     resources :movies, only: [:index, :show] do
-      resources :viewing_party, only: %i[new create]
+      resources :viewing_party, only: [:new, :create]
     end
   end
 end
