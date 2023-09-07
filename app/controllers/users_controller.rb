0074966140c1c9a5ba24673138d7f8181e4d932d
Artemy Gibson <artemygibson@gmail.com>
2023-09-07 14:37:44 -0700

    Introduce sessions by user_id, add logout method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @user = User.find(session[:user_id]) if session[:user_id]
   end
 
   def show
-    @user = User.find(params[:id])
+    begin
+      @user = User.find(session[:user_id])
+    rescue
+      redirect_to root_path
+      flash[:alert] = "Please log in or register to access your dashboard"
+    end
   end
 
   def register
@@ -12,12 +17,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
+    new_user = User.new(user_params)
+    if new_user.save
+      session[:user_id] = new_user.id 
       flash[:success] = 'Successfully registered.'
-      redirect_to user_path(@user)
+      redirect_to user_path(new_user)
     else
-      flash[:error] = @user.errors.full_messages.to_sentence
+      flash[:error] = new_user.errors.full_messages.to_sentence
       redirect_to register_path
     end
   end
@@ -28,18 +34,23 @@ class UsersController < ApplicationController
   
   def login
     user = User.find_by(email: params[:email])
-
     if user&.authenticate(params[:password])
+      session[:user_id] = user.id # Add to a SessionsController as Index after refactoring?
       flash[:success] = "Welcome, #{user.name}!"
-      redirect_to user_path(user)
+      redirect_to dashboard_path
     else 
       flash[:error] = "Sorry, those credentials are invalid"
       render :login_form
     end
   end
 
+  def logout
+    session.clear
+    redirect_to root_path
+  end
+
   def discover
-    @user = User.find(params[:user_id])
+
   end
 
   private
