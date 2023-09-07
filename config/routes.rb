2526635699010d15f74da39f5b8a5d708fa2c160
Artemy Gibson <artemygibson@gmail.com>
2023-09-07 16:41:50 -0700

    Edit routes so that none use the user_id anymore

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,16 @@ Rails.application.routes.draw do
   # root "articles#index"
   
   root 'users#index'
-  get 'login', to: 'users#login_form'
-  post 'login', to: 'users#login'
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login'
+  get '/logout', to: 'users#logout'
+  get '/discover', to: 'users#discover'
+  get '/dashboard', to: 'users#show'
 
-  get '/register', to: 'users#register', as: 'register_user'
+  get '/register', to: 'users#new'
   post '/register', to: 'users#create'
 
-  resources :users, only: [:show] do
-    get '/discover', to: 'users#discover'
-    resources :movies, only: [:index, :show] do
-      resources :viewing_party, only: [:new, :create]
-    end
+  resources :movies, only: [:index, :show] do
+    resources :viewing_party, only: [:new, :create]
   end
 end
