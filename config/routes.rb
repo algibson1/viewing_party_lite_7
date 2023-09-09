48a2532e8ec1ae6ecd753a27e815680f8a6f91b8
Artemy Gibson <artemygibson@gmail.com>
2023-09-09 15:00:53 -0700

    Redirect /discover to movies#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do  
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  
   root 'users#index'
   get '/login', to: 'users#login_form'
   post '/login', to: 'users#login'
   get '/logout', to: 'users#logout'
-  get '/discover', to: 'users#discover'
+  get '/discover', to: 'movies#index'
   get '/dashboard', to: 'users#show'
 
   get '/register', to: 'users#new'
@@ -17,4 +13,5 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :show] do
     resources :viewing_party, only: [:new, :create]
   end
+  
 end
